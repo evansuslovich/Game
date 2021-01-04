4bcda26505d7fe05ccaf9c728f3860a44ddc6018
evansuslovich <evan.suslovich@lincolnps.org>
2021-01-04 17:51:48 -0500

    Main file

---
+++ Game.java
@@ -0,0 +1,14 @@
+import javax.swing.*; 
+
+public class Game extends JFrame{
+    private static final long serialVersionUID = 1L;
+
+
+    public Game() {
+
+    }
+    public static void main(String[] args){
+        new Game();
+
+    }
+}
