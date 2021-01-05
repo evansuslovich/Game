f4f0a14f1df9c2fcaed8eff0d0cd8ed585b23e54
evansuslovich <evan.suslovich@lincolnps.org>
2021-01-04 19:19:07 -0500

    Added JPanel panel. This contains a mouse listener.

--- Game.java
+++ Game.java
@@ -3,12 +3,19 @@ import javax.swing.*;
 public class Game extends JFrame{
     private static final long serialVersionUID = 1L;
 
+    
 
-    public Game() {
+    private Panel panel = new Panel(); 
 
+    public Game(){
+        setSize(450,300); 
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        add(panel); 
+        setVisible(true); 
     }
     public static void main(String[] args){
         new Game();
 
     }
-}
+} 
