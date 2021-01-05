6822448f5e8ac91fe2bf66c128e4d9875aa909aa
evansuslovich <evan.suslovich@lincolnps.org>
2021-01-04 19:34:13 -0500

    I am going to start making the shooting aspect of the game

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.*; 
 import javax.swing.*; 
 
 public class Game extends JFrame{
@@ -5,13 +7,24 @@ public class Game extends JFrame{
 
     
 
-    private Panel panel = new Panel(); 
+    private Panel panel = new Panel();
+    
+    JPanel left = new JPanel(); 
+    JPanel right = new JPanel(); 
 
     public Game(){
         setSize(450,300); 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        left.setBackground(Color.blue); 
+        right.setBackground(Color.red); 
+
+        panel.add(left); 
+        panel.add(right); 
 
         add(panel); 
+        
         setVisible(true); 
     }
     public static void main(String[] args){
