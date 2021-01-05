e4876b661d674cd9741fbbbb57180bfe2a1b1f97
evansuslovich <evan.suslovich@lincolnps.org>
2021-01-04 21:46:19 -0500

    Panel.java is updated with everything done in FallingRect.java

--- Panel.java
+++ Panel.java
@@ -1,13 +1,48 @@
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.Graphics;
+import java.awt.Color; 
+
 import javax.swing.*;
+
+import java.util.Random; 
+
 
 public class Panel extends JPanel implements MouseListener {
     private static final long serialVersionUID = 1L;
 
+    private int x = 0; 
+    private int y = 0; 
+
+    private int r1 = 0; 
+    private int r2 = 0; 
+    private int r3 = 0; 
+
+    private Random rand = new Random(); 
     public Panel(){
         addMouseListener(this);
     }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponents(g);
+
+        for(int i = 0; i < 10; i++){
+            x = rand.nextInt(400); 
+
+            r1 = rand.nextInt(256); 
+            r2 = rand.nextInt(256); 
+            r3 = rand.nextInt(256); 
+
+            draw(g);
+        }
+    }
+
+    public void draw(Graphics g) {
+        System.out.println(x + " " + y); 
+        g.setColor(new Color(r1,r2,r3));
+        g.fillRect(x,y,20,20); 
+    }
+
 
     @Override
     public void mouseClicked(MouseEvent e) {
