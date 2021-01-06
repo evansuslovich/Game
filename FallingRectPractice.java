3df6bb0b266e1cde94bb1b268854e91dabbe7a53
evansuslovich <evan.suslovich@lincolnps.org>
2021-01-05 19:46:54 -0500

    Making an array for rectangles

--- FallingRectPractice.java
+++ FallingRectPractice.java
@@ -1,24 +1,46 @@
-import java.util.Timer;
-import java.util.TimerTask; 
-public class FallingRectPractice extends TimerTask {
-    private int counter = 0; 
-    private Timer timer = new Timer();
+import javax.swing.*;
+import java.awt.Rectangle; 
+import java.awt.Graphics; 
+import java.awt.Color; 
+
+import java.util.Random; 
+
+public class FallingRectPractice extends JPanel{
+    private static final long serialVersionUID = 1L; 
+
+    private Rectangle[] rectangles = new Rectangle[25]; 
 
 
-
-    public void run(){
-        if(counter == 5){
-            timer.cancel();;
-        }
-        System.out.println("Hello World"); 
-        counter++; 
+    public FallingRectPractice(){
+        JFrame frame = new JFrame(); 
+        frame.setSize(400,400); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        fillArray(); 
+
+        frame.add(this); 
+        frame.setVisible(true); 
+    }
+    
+    public void fillRectangles(){
+        Random rand = new Random(); 
+        int x = 
+        for(int i = 0; i < rectangles.length; i++){
+
+        }
+    }
+    public void paint(Graphics g){
+        super.paint(g);
+
+        g.setColor(Color.black); 
+        //g.fillRect(rectangle1.x,rectangle1.y, rectangle1.width, rectangle1.height);
 
     }
+
+
+
     public static void main(String[] args){
-        //Timer timer = new Timer(); 
-        //timer.schedule(new FallingRectPractice(), 0, 1000);  
+        new FallingRectPractice(); 
     }
-   
-
 }
+  
