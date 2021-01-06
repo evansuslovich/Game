5219c40c4f2cd52afb502880ec451141679927d5
evansuslovich <evan.suslovich@lincolnps.org>
2021-01-06 13:09:22 -0500

    Working on the timer

--- FallingRectPractice.java
+++ FallingRectPractice.java
@@ -1,24 +1,25 @@
 import javax.swing.*;
-import java.awt.Rectangle; 
-import java.awt.Graphics; 
-import java.awt.Color; 
 
-import java.util.Random; 
+import java.awt.*;
+import java.awt.event.*;
 
-public class FallingRectPractice extends JPanel{
+import java.util.Random;
+
+public class FallingRectPractice extends JPanel implements ActionListener{
     private static final long serialVersionUID = 1L; 
 
-    private Rectangle[] rectangles = new Rectangle[1]; 
+    private Rectangle[] rectangles = new Rectangle[10]; 
     private JFrame frame; 
+    private int counter = 0; 
 
+    Timer time;
     public FallingRectPractice(){
         frame = new JFrame(); 
         frame.setSize(400,400); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         fillRectangles(); 
-        intersect();
-        //moveRectangles();  
+        //intersect();
     
 
         frame.add(this); 
@@ -26,23 +27,31 @@ public class FallingRectPractice extends JPanel{
     }
     
     public void fillRectangles(){
-        /*Random rand = new Random(); 
+        Random rand = new Random(); 
    
         for(int i = 0; i < rectangles.length; i++){
             int x = rand.nextInt(400); 
             int y = rand.nextInt(100);  
 
             rectangles[i] = new Rectangle(x,y,10,10);
-            */ 
-        rectangles[0] = new Rectangle(10,10,10,10);
-        
+        }
     }
 
     public void moveRectangles(){
+        counter++; 
+       
         for(int i = 0; i < rectangles.length; i++){
-            rectangles[i].setBounds(rectangles[i].x,rectangles[i].y + 5,rectangles[i].width, rectangles[i].height);
-            repaint(); 
+            if(counter <= 3){
+                if(rectangles[i].getY() < 400){
+                    rectangles[i].setLocation(rectangles[i].x, rectangles[i].y + 5);
+                }
+                repaint(); 
+            }
+            else{
+                time.stop(); 
+            }
         }
+        
     }
 
     public void paint(Graphics g){
@@ -58,10 +67,21 @@ public class FallingRectPractice extends JPanel{
             g.setColor(new Color(r1,r2,r3)); 
             g.fillRect(rectangles[i].x,rectangles[i].y, rectangles[i].width, rectangles[i].height);
         }
+        
+        time = new Timer(1000, this);
+        time.start();
+
 
         //g.setColor(Color.black); 
         //g.fillRect(0,0, frame.getContentPane().getWidth(), (int)(frame.getContentPane().getHeight()*.26)); 
     }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        moveRectangles(); 
+    }
+    
 
     
 
@@ -84,14 +104,7 @@ public class FallingRectPractice extends JPanel{
     }
 
     public static void main(String[] args){
-        FallingRectPractice practice = new FallingRectPractice();
-
-        FallingRectPractice practice2 = new FallingRectPractice(); 
-        for(int i = 0; i < 10; i++){
-            practice2.moveRectangles();
-        }
-
-
+        new FallingRectPractice(); 
     }
 }
   
