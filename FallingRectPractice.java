2f04e4842cc6e3cc1c99c68096633a9da29669a9
evansuslovich <evan.suslovich@lincolnps.org>
2021-01-05 20:15:34 -0500

    I am going to start making them fall now

--- FallingRectPractice.java
+++ FallingRectPractice.java
@@ -8,7 +8,7 @@ import java.util.Random;
 public class FallingRectPractice extends JPanel{
     private static final long serialVersionUID = 1L; 
 
-    private Rectangle[] rectangles = new Rectangle[25]; 
+    private Rectangle[] rectangles = new Rectangle[10]; 
 
 
     public FallingRectPractice(){
@@ -24,17 +24,12 @@ public class FallingRectPractice extends JPanel{
     
     public void fillRectangles(){
         Random rand = new Random(); 
-        int x = 0; 
-        int y = 0; 
-
-        int width = 10; 
-        int height = 10; 
-
+   
         for(int i = 0; i < rectangles.length; i++){
-            x = rand.nextInt(400); 
-            y = rand.nextInt(400); 
+            int x = rand.nextInt(400); 
+            int y = rand.nextInt(100); 
 
-            rectangles[i] = new Rectangle(x,y,width,height);
+            rectangles[i] = new Rectangle(x,y,10,10);
 
         }
     }
@@ -52,8 +47,6 @@ public class FallingRectPractice extends JPanel{
             g.fillRect(rectangles[i].x,rectangles[i].y, rectangles[i].width, rectangles[i].height);
         }
     }
-
-
 
     public static void main(String[] args){
         new FallingRectPractice(); 
