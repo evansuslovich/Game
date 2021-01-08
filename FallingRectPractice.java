5d87cb240c09e54cf662e0ac29ca05f1e0150afd
evansuslovich <evan.suslovich@lincolnps.org>
2021-01-07 20:04:31 -0500

    I feel comfortable to move to my original class with everything that I have learned. I want to allocate my time to focus on shooting from the bottom center of the panel (the artillery)

--- FallingRectPractice.java
+++ FallingRectPractice.java
@@ -8,28 +8,23 @@ import java.awt.event.*;
 import java.util.Random;
 
 import javax.swing.Timer; 
-//import java.util.TimerTask;
-//import java.util.Timer;
 
 public class FallingRectPractice extends JPanel implements ActionListener{
+
     private static final long serialVersionUID = 1L; 
-
     static Rectangle[] rectangles = new Rectangle[10]; 
-    
     private JFrame frame; 
-
     Timer timer;
-    //TimerTask rectanglesTask; 
-
-
     
+
+
     public FallingRectPractice(){
         frame = new JFrame(); 
-        frame.setSize(400,400); 
+        frame.setSize(500,400); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         createRectangles(); 
-        
-        timer = new Timer(1000, this);
+
+        timer = new Timer(100, this);
         timer.setRepeats(true);
         timer.start();
         
@@ -42,23 +37,34 @@ public class FallingRectPractice extends JPanel implements ActionListener{
     public void createRectangles(){
         Random rand = new Random(); 
         for(int i = 0; i < rectangles.length; i++){
-            int x = rand.nextInt(400); 
-            int y = rand.nextInt(100);  
+            int x = rand.nextInt(500); 
+            int y = rand.nextInt(25);  
             rectangles[i] = new Rectangle(x,y,10,10);
         }
     }
     
     // move the rectangles down 5 pixels
     public void moveRectangles(){
-            for(int i = 0; i < rectangles.length; i++){
-            if(rectangles[i].getY() < 400){
-                rectangles[i].setLocation(rectangles[i].x, rectangles[i].y + 5);
-                repaint(); 
+        int index = 0; 
+            for(int i = 0; i < rectangles.length; i++)
+            {
+                if(rectangles[i].getY() < 350){
+                    rectangles[i].setLocation(rectangles[i].x, rectangles[i].y + 5);
+                    repaint(); 
+                }
+                else
+                {
+                    stopRectangles(index); 
+                }
             }
         }
+    public void stopRectangles(int i){
+        rectangles[i].setLocation(rectangles[i].x, rectangles[i].y);
+        repaint();
     }
 
 
+    // abstract method that paints the graphics 
     public void paint(Graphics g){
         super.paint(g);
         Random rand = new Random(); 
@@ -72,13 +78,14 @@ public class FallingRectPractice extends JPanel implements ActionListener{
             g.fillRect(rectangles[i].x,rectangles[i].y, rectangles[i].width, rectangles[i].height);
         }
     
-        //g.setColor(Color.black); 
-        //g.fillRect(0,0, frame.getContentPane().getWidth(), (int)(frame.getContentPane().getHeight()*.26)); 
+        g.setColor(Color.black); 
+        g.fillRect(0,0, frame.getContentPane().getWidth(), (int)(frame.getContentPane().getHeight()*.26)); 
     
 
     }
 
 
+    // time abstract class that moves the rectangles
     @Override
     public void actionPerformed(ActionEvent e){
         moveRectangles(); 
