dca05af0732f58b1275f803c037bceaa1b6fdebc
evansuslovich <evan.suslovich@lincolnps.org>
2021-01-07 20:58:50 -0500

    Added a mouse listener

--- Artillery.java
+++ Artillery.java
@@ -2,8 +2,14 @@ import java.awt.*;
 import javax.swing.*; 
 import java.awt.Color; 
 
+import java.awt.event.MouseEvent; 
+import java.awt.event.MouseListener; 
 
-public class Artillery extends JPanel{
+import java.awt.event.ActionListener;
+
+
+
+public class Artillery extends JPanel implements ActionListener{
 
     private static final long serialVersionUID = 1L;
     private JFrame frame; 
@@ -14,6 +20,34 @@ public class Artillery extends JPanel{
         frame.setSize(400,400); 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
         
+        this.addMouseListener(new MouseListener(){
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println(e.getPoint());  
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+
+        });
 
         frame.add(this); 
         frame.setVisible(true); 
@@ -21,17 +55,17 @@ public class Artillery extends JPanel{
 
 
     public void paint(Graphics g){
+        // back panel 
         g.setColor(new Color(0,0,128));
         g.fillRect(0,(int)(frame.getContentPane().getHeight()*.90), frame.getContentPane().getWidth(), 40);
 
+        // artillery 
         g.setColor(new Color(75,83,32)); 
         g.fillRect(artillery.x, artillery.y, artillery.width, artillery.height); 
 
     }
 
-    
 
-   
     public static void main(String[] args){
         new Artillery(); 
     }
