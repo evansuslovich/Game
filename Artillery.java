2a554a3623e8b6a7ac5873fe169455fc0dcce186
evansuslovich <evan.suslovich@lincolnps.org>
2021-01-07 20:59:36 -0500

    Added abstract class

--- Artillery.java
+++ Artillery.java
@@ -3,24 +3,22 @@ import javax.swing.*;
 import java.awt.Color; 
 
 import java.awt.event.MouseEvent; 
-import java.awt.event.MouseListener; 
-
+import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
-
-public class Artillery extends JPanel implements ActionListener{
+public class Artillery extends JPanel implements ActionListener {
 
     private static final long serialVersionUID = 1L;
-    private JFrame frame; 
-    private Rectangle artillery = new Rectangle(200,350,15,15); 
+    private JFrame frame;
+    private Rectangle artillery = new Rectangle(200, 350, 15, 15);
 
-    public Artillery(){
-        frame = new JFrame(); 
-        frame.setSize(400,400); 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        
-        this.addMouseListener(new MouseListener(){
+    public Artillery() {
+        frame = new JFrame();
+        frame.setSize(400, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.addMouseListener(new MouseListener() {
 
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -29,7 +27,7 @@ public class Artillery extends JPanel implements ActionListener{
 
             @Override
             public void mousePressed(MouseEvent e) {
-                System.out.println(e.getPoint());  
+                System.out.println(e.getPoint());
             }
 
             @Override
@@ -49,24 +47,27 @@ public class Artillery extends JPanel implements ActionListener{
 
         });
 
-        frame.add(this); 
-        frame.setVisible(true); 
-    }   
+        frame.add(this);
+        frame.setVisible(true);
+    }
 
+    public void paint(Graphics g) {
+        // back panel
+        g.setColor(new Color(0, 0, 128));
+        g.fillRect(0, (int) (frame.getContentPane().getHeight() * .90), frame.getContentPane().getWidth(), 40);
 
-    public void paint(Graphics g){
-        // back panel 
-        g.setColor(new Color(0,0,128));
-        g.fillRect(0,(int)(frame.getContentPane().getHeight()*.90), frame.getContentPane().getWidth(), 40);
-
-        // artillery 
-        g.setColor(new Color(75,83,32)); 
-        g.fillRect(artillery.x, artillery.y, artillery.width, artillery.height); 
+        // artillery
+        g.setColor(new Color(75, 83, 32));
+        g.fillRect(artillery.x, artillery.y, artillery.width, artillery.height);
 
     }
 
+    public static void main(String[] args) {
+        new Artillery();
+    }
 
-    public static void main(String[] args){
-        new Artillery(); 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
